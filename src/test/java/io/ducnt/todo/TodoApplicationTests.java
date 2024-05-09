a8dcb76df7166e142c690b7bfe987cbe3f0554b1
trungducng0410 <ngtrungducyb@gmail.com>
2024-05-09 17:18:08 +1000

    init

---
+++ src/test/java/io/ducnt/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ducnt.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
